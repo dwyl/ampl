f23203a5c33b6e5ccb6cb5a7879a4f8e772d3815
nelsonic <contact.nelsonic@gmail.com>
2014-10-12 21:48:53 +0100

    getPosts(function(err,posts){}) array of posts

--- helpers.js
+++ helpers.js
@@ -1,48 +1,70 @@
 var fs = require('fs');
 var path = require('path');
-var util = require('util');
+
+
 // date-ordered (descending) list of posts
-// get list of markdown (.md) files in ./posts dir
-var files = fs.readdirSync(path.resolve('./posts'));
-var posts = {};
-var errors = [];
+function getPosts(callback) {
+  var files = fs.readdirSync(path.resolve('./posts'));
+  var posts = [];  // date-ordered array of posts
+  var errors = []; // an array of all errors
 
-console.log(files.length, files)
-// reduce list of files to only markdown
-var mds = files.map(function(file) {
-  if(path.extname(file).toLowerCase() === '.md') {
-    return file;
-  }
-});
-// remove undefined, null, etc. - http://stackoverflow.com/questions/281264
-mds = mds.filter(function(n){ return n });
-
-// number of markdown files (posts)
-
-remaining = mds.length;
-console.log(remaining, mds);
-
-mds.map(function(file){
-  var filepath = path.resolve('./posts/'+file);
-  fs.stat(filepath, function(err, stats){
-    errors.push(err);
-    // console.log(stats);
-    if(!err){
-      fs.readFile(filepath, 'utf8', function(error, data){
-        posts[file] = {};
-        posts[file].lastmod = stats.mtime;
-        posts[file].data = data;
-        // console.log(data);
-        // console.log(remaining--);
-        if(--remaining === 0){
-          console.log('>>> finished');
-          // this is where the callback goes
-          console.log(util.inspect(posts));
-        }
-      });
-      // console.log(stats);
+  // reduce list of files to only markdown
+  var mds = files.map(function(file) {
+    if(path.extname(file).toLowerCase() === '.md') {
+      return file;
     }
   });
-});
+  // remove undefined, null, etc. - http://stackoverflow.com/questions/281264
+  mds = mds.filter(function(n){ return n });
 
-// loop through all the .md files in ./posts
+  // number of markdown files (posts)
+  remaining = mds.length; // we decrement this below
+
+  mds.map(function(file){
+    var filepath = path.resolve('./posts/'+file);
+    fs.stat(filepath, function(err, stats){
+      errors.push(err);
+      // console.log(stats);
+      if(!err) {
+
+        fs.readFile(filepath, 'utf8', function(error, data){
+          errors.push(error);
+          var post = {path:filepath};
+          post.title = getTitle(data);
+          post.intro = getIntro(data);
+          post.mtime = stats.mtime;
+          post.data = data;
+          posts.push(post);
+
+          if(--remaining === 0){
+            // sorts potsts by date decending (newest first)
+            posts.sort(function(a,b){
+              return b.mtime - a.mtime;
+            }); // see: http://stackoverflow.com/questions/10123953
+
+            callback(errors, posts);
+          }
+        });
+      }
+    });
+  });
+}
+
+// extract post title
+function getTitle (post) {
+  return post.toString().split('\n')[0].replace('#', '').trim();
+}
+
+// extract the first few lines of the post
+function getIntro (post, lineLimit) {
+  lineLimit = lineLimit || 5;
+  // separate lines in markdown
+  var lines = post.toString().split('\n');
+  var end = lines.length > lineLimit ? lineLimit : lines.length;
+  // assumes first line is the title
+  return lines.slice(0, end).join('\n');
+}
+
+module.exports = {
+  getPosts : getPosts
+};
