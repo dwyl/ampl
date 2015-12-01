7327f1284ae5dd07aa67fd3da97926cf8aff220a
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-12-01 13:05:20 +0000

    beginning to convert markdown to amp-html #24

---
+++ markdown-amp.js
@@ -0,0 +1,11 @@
+var Remarkable = require('remarkable');
+// var fs         = require('fs');
+// var post1      = fs.readFileSync(__dirname + '/../posts/post1.md');
+
+
+// console.log(md.render(post1.toString()));
+
+var md = new Remarkable('full');
+module.exports = function parse(mdString) {
+  return md.render(mdString);
+};
