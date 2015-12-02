0d777b27b9b75e9c2b45eca20042a2b6fcfa41df
Conor C <Conorc1000@gmail.com>
2015-12-02 18:06:52 +0000

    front end tests #63

---
+++ editor.js
@@ -0,0 +1,11 @@
+var editor = {};
+
+editor.addTen =  function(a){
+return a+10;
+};
+
+editor.inputValue =  function(){
+  var input = document.getElementById('title').value;
+  console.log(input);
+  return input;
+};
