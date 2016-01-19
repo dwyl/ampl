2276bb3ac2d206f2891f0f21ee327daf90989188
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-19 18:58:07 +0000

    example server

--- example/app.js
+++ example/app.js
@@ -1,31 +1,41 @@
-var Hapi   = require('hapi');
-var Inert  = require('inert'); // serve static content
-var Path   = require('path');  // node core module
-// var Basic   = require('hapi-auth-basic');
-// var AuthJWT = require('hapi-auth-jwt2')
-// var Joi     = require('joi');
-// var lout    = require('lout');
-// var ES      = require('esta');  // https://github.com/nelsonic/esta
-var port   = process.env.PORT || 3000; // heroku define port or use 3000
+var fs = require('fs');
+var path = require('path');
+
+var Hapi = require('hapi');
+var Inert = require('inert');
+
+var ampl = require('../lib/ampl.js');
+
+var port = process.env.PORT || 3000;
 var server = new Hapi.Server();
 
 server.connection({ port: port });
-console.log('Path:', Path.normalize(__dirname + '/../'));
-// var routes = require('./routes.js');
+
+// console.log(typeof ampl);
+// console.log(Object.keys(ampl));
 
 server.register(Inert, function (err) {
   server.route( {
       method: 'GET',
       path: '/{param*}',
       handler: {
-        directory: { path: Path.normalize(__dirname + '/../.site/') }
+        directory: {
+          path: path.join(__dirname, 'public'),
+          listing: true
+        }
       }
     });
-  // server.route(routes);
+  server.route({
+    method: 'POST',
+    path: '/convert',
+    handler: function(request, reply) {
+      ampl.parse(request.payload.md, function(ampHtml) {
+        return reply(ampHtml);
+      });
+    }
+  })
 });
 
 server.start(function(){
-  console.log('Now Visit: http://localhost:'+port);
+  console.log('server listening on port', port);
 });
-
-module.exports = server;
