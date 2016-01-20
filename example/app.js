5fe5092163cec38ee6b6ed5feeabb289ac43c2ce
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-20 12:35:53 +0000

    example server now hasstyle

--- example/app.js
+++ example/app.js
@@ -6,13 +6,12 @@ var Inert = require('inert');
 
 var ampl = require('../lib/ampl.js');
 
+var css = fs.readFileSync(path.join(__dirname, './amp.css'));
+
 var port = process.env.PORT || 3000;
 var server = new Hapi.Server();
 
 server.connection({ port: port });
-
-// console.log(typeof ampl);
-// console.log(Object.keys(ampl));
 
 server.register(Inert, function (err) {
   server.route( {
@@ -29,7 +28,7 @@ server.register(Inert, function (err) {
     method: 'POST',
     path: '/convert',
     handler: function(request, reply) {
-      ampl.parse(request.payload.md, function(ampHtml) {
+      ampl.parse(request.payload.md, css, function(ampHtml) {
         return reply(ampHtml);
       });
     }
