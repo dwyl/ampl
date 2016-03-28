2131ff746f9566857781b3a7fe5123ef1147bb0f
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-03-28 17:26:10 +0100

    add test to check user can overwrite head

--- src/templates.js
+++ src/templates.js
@@ -1,21 +1,20 @@
 const createAmpHeader = ({
   headOverride, style, canonicalUrl, title, extraHeadHTML
-}) => typeof headOverride === 'string' ? headOverride : (
-`<!doctype html>
-  <html amp>
-    <head>
-      <meta charset="utf-8">
-      <link rel="canonical" href="${canonicalUrl || ""}" >
-      <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-      <style amp-custom>
-        ${style || ""}
-      </style>
-      <style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>
-      <script async src="https://cdn.ampproject.org/v0.js"></script>
-      <title>${title || ""}</title>
-      ${extraHeadHTML || ""}
-    </head>`
-  );
+}) => `
+<!doctype html>
+<html amp>
+  <head>${typeof headOverride === 'string' ? headOverride : `
+    <meta charset="utf-8">
+    <link rel="canonical" href="${canonicalUrl || ""}" >
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style amp-custom>
+      ${style || ""}
+    </style>
+    <style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <title>${title || ""}</title>
+    ${extraHeadHTML || ""}
+  `}</head>`;
 
 export const createAmpPage = (contentHTML, opts) => (
 `${createAmpHeader(opts)}
--- test/templates.test.js
+++ test/templates.test.js
@@ -1,14 +1,29 @@
 import test from 'tape';
 
-import { createAmpPage } from '../src/templates.js'
+import { createAmpPage } from '../src/templates.js';
+
+const getFirstInner = (tag, html) => html
+  .split(`<${tag}>`)[1]
+  .split(`</${tag}>`)[0];
+
+test('headOverride', t => {
+  const replacementHead = 'replace standard amp head with me';
+  const ampHtml = createAmpPage('soome html', {headOverride: replacementHead});
+  const expected = replacementHead;
+  const actual = ampHtml.split('')
+  t.equal(getFirstInner('head', ampHtml), replacementHead, 'head replaced');
+  t.end();
+});
 
 test("template.js", t => {
   const style = "here is some styling";
   const html = "<this is not a tag>";
   const ampHtml = createAmpPage(html, {style});
+  console.log('###', ampHtml);
   t.ok(ampHtml.indexOf(html) !== -1, "html inserted into amp template");
   t.ok(ampHtml.indexOf(style) !== -1, "style inserted into amp template");
-  t.ok(ampHtml.split('\n')[1].split(' ')[3].indexOf('amp') === 0,
+  console.log(ampHtml.split('\n')[2]);
+  t.ok(ampHtml.split('\n')[2].split(' ')[1].indexOf('amp') === 0,
     'html tag contains amp keyword');
   t.end();
 });
